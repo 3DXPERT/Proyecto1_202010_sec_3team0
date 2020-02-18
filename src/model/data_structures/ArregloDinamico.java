d12d00b36be7b596b0e32e53b294424828acab9f
3DXPERT <da.castroc1@uniandes.edu.co>
2020-02-17 23:16:04 -0500

    first commit

---
+++ src/model/data_structures/ArregloDinamico.java
@@ -0,0 +1,92 @@
+package model.data_structures;
+
+/**
+ * 2019-01-23
+ * Estructura de Datos Arreglo Dinamico de Strings.
+ * El arreglo al llenarse (llegar a su maxima capacidad) debe aumentar su capacidad.
+ * @author Fernando De la Rosa
+ *
+ */
+public class ArregloDinamico<T> implements IArregloDinamico<T> {
+		/**
+		 * Capacidad maxima del arreglo
+		 */
+        private int tamanoMax;
+		/**
+		 * Numero de elementos presentes en el arreglo (de forma compacta desde la posicion 0)
+		 */
+        private int tamanoAct;
+        /**
+         * Arreglo de elementos de tamaNo maximo
+         */
+        private String elementos[ ];
+
+        /**
+         * Construir un arreglo con la capacidad maxima inicial.
+         * @param max Capacidad maxima inicial
+         */
+		public ArregloDinamico( int max )
+        {
+               elementos = new String[max];
+               tamanoMax = max;
+               tamanoAct = 0;
+        }
+        
+		public void agregar( T dato )
+        {
+               if ( tamanoAct == tamanoMax )
+               {  // caso de arreglo lleno (aumentar tamaNo)
+                    tamanoMax = 2 * tamanoMax;
+                    String [ ] copia = elementos;
+                    elementos = new String[tamanoMax];
+                    for ( int i = 0; i < tamanoAct; i++)
+                    {
+                     	 elementos[i] = copia[i];
+                    } 
+            	    System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
+               }	
+               elementos[tamanoAct] = dato;
+               tamanoAct++;
+       }
+
+		public int darCapacidad() {
+			return tamanoMax;
+		}
+
+		public int darTamano() {
+			return tamanoAct;
+		}
+
+		public String darElemento(int i) {
+			
+			return elementos[i];
+		}
+
+		public String buscar(String dato) {
+			String elemento = null;
+			for ( int i = 0; i < tamanoAct; i++)
+            {
+             	 if (elementos[i].compareTo(dato) == 1) {
+             		elemento = elementos[i];
+             	 }
+            } 
+			return elemento;
+		}
+
+		public String eliminar(T dato) {
+			String elemento = null;
+			boolean encontrado = false;
+			int posicion = -1;
+			while (encontrado = false)
+			for ( int i = 0; i < tamanoAct; i++)
+            {
+             	 if (elementos[i].compareTo(dato) == 1) {
+             	elementos[i] = null;
+             	encontrado = true;
+             	 }
+            } 
+			return elementos[posicion];
+			
+		}
+
+}
